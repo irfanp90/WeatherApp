87fc0be0267c5781b5ecf67c4ad946a29c8ae440
irfanp90 <irfanp90@gmail.com>
2019-08-29 22:46:30 -0500

    update

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolio");
-
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
