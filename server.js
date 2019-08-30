2102711fdee4a912fe171830892002fb7637fd96
irfanp90 <irfanp90@gmail.com>
2019-08-29 22:59:54 -0500

    update

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build',));
-});
+
 
 // Start the API server
 app.listen(PORT, function() {
