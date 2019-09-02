bae5991c6ffd3dee4cf437d05ae71d763b4ed059
irfanp90 <irfanp90@gmail.com>
2019-09-02 11:42:05 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
