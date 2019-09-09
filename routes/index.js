303cd57c251fe5fe84f0f5dc79cca1c49b828b7c
irfanp90 <irfanp90@gmail.com>
2019-09-09 11:23:34 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const router = require("express").Router();
 const path = require("path");
+const router = require("express").Router();
 
 
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../clint/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 
 module.exports = router;
